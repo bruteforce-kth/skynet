64e58106de947752e6eb591d2644f6e0246ae763
glindste <gustaflindstedt@gmail.com>
2013-09-24 16:49:47 +0200

    added separate run file

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+for i in {1..1}
+do
+    #./main < maps/00_sample.in
+    #./main < maps/01_sample.in
+    #./main < maps/02_sample.in
+    #./main < maps/sokoban1
+    #./main < maps/sokoban2
+    ./main < maps/sokoban3
+done
+
