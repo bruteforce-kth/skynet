cbf2e6e0bd2a4e97aa97ea7db1d673ddf5770f04
glindste <gustaflindstedt@gmail.com>
2013-09-25 15:59:55 +0200

    more run functionality

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash 
 
-for i in {1..1}
-do
-    #./main < maps/00_sample.in
-    #./main < maps/01_sample.in
-    #./main < maps/02_sample.in
-    #./main < maps/sokoban1
-    #./main < maps/sokoban2
+if [ $# -lt 1 ]; then
     ./main < maps/sokoban3
-done
+    exit
+fi
 
+case "$1" in
+    1) ./main < maps/sokoban1;;
+    2) ./main < maps/sokoban2;;
+    3) ./main < maps/sokoban3;;
+    00) ./main < maps/00_sample.in;;
+    01) ./main < maps/01_sample.in;;
+    02) ./main < maps/02_sample.in;;
+    *) ./main < maps/sokoban3;;
+esac
