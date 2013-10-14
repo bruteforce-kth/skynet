ad1f22029a6fdc184bbc93e7ad384a78babc7c48
mrunelov <martin.runelov@gmail.com>
2013-10-14 21:47:28 +0200

    tired of discarding changes here

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -2,7 +2,7 @@
 
 make
 
-for i in {1..3}
+for i in {1..1}
 do
     #./main < maps/00_sample.in
     #./main < maps/01_sample.in
@@ -12,8 +12,8 @@ do
     #./main < maps/sokoban3
     #./main < maps/20_maps/F6
     #./main < maps/simple
-    ./main < maps/simple5
-    #./main < maps/test100/test001.in
+    #./main < maps/simple5
+    ./main < maps/test100/test001.in
     #./main < maps/dynDeadlockMap
 done
 
